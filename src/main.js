61ceb05a7db951d6839421e2de0f4d53f5203d27
Clementine  Oldfield <clementine.oldfield@gmail.com>
2019-03-21 14:25:02 +1100

    Cleaned up Javascript

--- src/main.js
+++ src/main.js
@@ -16,18 +16,6 @@ window.onload = function() {
 
 var isScrolling = false;
 
-var walkingUp = new Image();
-walkingUp.setAttribute('id', 'little-clem')
-walkingUp.src = 'assets/walking-up.gif';
-
-var walkingDown = new Image();
-walkingDown.setAttribute('id', 'little-clem')
-walkingDown.src = 'assets/walking-down.gif';
-
-var standing = new Image();
-standing.setAttribute('id', 'little-clem')
-standing.src = 'assets/little-clem.png';
-
 var still = document.getElementById('sprite-still');
 var wDown = document.getElementById('sprite-walk-down');
 var wUp = document.getElementById('sprite-walk-up');
@@ -37,8 +25,6 @@ var lastScrollTop = pageYOffset;
 window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
     var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
     
-    var littleClem = document.getElementById('little-clem');
-
     // Clear our timeout throughout the scroll
     window.clearTimeout( isScrolling );
 
@@ -47,9 +33,7 @@ window.addEventListener("scroll", function(){ // or window.addEventListener("scr
 
 		// Run the callback
         console.log( 'Scrolling has stopped.' );
-        littleClem.replaceWith(standing);
-        littleClem = standing;
-
+        
         still.classList.add('active');
         wDown.classList.remove('active');
         wUp.classList.remove('active');
@@ -59,8 +43,6 @@ window.addEventListener("scroll", function(){ // or window.addEventListener("scr
    if (st > lastScrollTop){
       // downscroll code
       console.log("scrolling down");
-      littleClem.replaceWith(walkingUp);
-      littleClem = walkingUp;
 
       still.classList.remove('active');
       wDown.classList.remove('active');
@@ -68,8 +50,6 @@ window.addEventListener("scroll", function(){ // or window.addEventListener("scr
    } else if (st < lastScrollTop) {
       // upscroll code
       console.log("scrolling up");
-      littleClem.replaceWith(walkingDown);
-      littleClem = walkingDown;
 
       still.classList.remove('active');
       wDown.classList.add('active');
