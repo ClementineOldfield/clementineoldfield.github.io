7fd2e918733d08eff4b1545556b5ebc2e2093ac5
Clementine  Oldfield <clementine.oldfield@gmail.com>
2019-03-21 14:22:12 +1100

    Fixed animation on mobile

--- src/main.js
+++ src/main.js
@@ -14,8 +14,6 @@ window.onload = function() {
     menu.classList.remove('active');
 }
 
-var lastScrollTop = 0;
-
 var isScrolling = false;
 
 var walkingUp = new Image();
@@ -30,9 +28,12 @@ var standing = new Image();
 standing.setAttribute('id', 'little-clem')
 standing.src = 'assets/little-clem.png';
 
-
+var still = document.getElementById('sprite-still');
+var wDown = document.getElementById('sprite-walk-down');
+var wUp = document.getElementById('sprite-walk-up');
 
 // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+var lastScrollTop = pageYOffset;
 window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
     var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
     
@@ -49,18 +50,30 @@ window.addEventListener("scroll", function(){ // or window.addEventListener("scr
         littleClem.replaceWith(standing);
         littleClem = standing;
 
-	}, 100);
+        still.classList.add('active');
+        wDown.classList.remove('active');
+        wUp.classList.remove('active');
+
+	}, 65);
    
    if (st > lastScrollTop){
       // downscroll code
       console.log("scrolling down");
       littleClem.replaceWith(walkingUp);
       littleClem = walkingUp;
-   } else {
+
+      still.classList.remove('active');
+      wDown.classList.remove('active');
+      wUp.classList.add('active');
+   } else if (st < lastScrollTop) {
       // upscroll code
       console.log("scrolling up");
       littleClem.replaceWith(walkingDown);
       littleClem = walkingDown;
+
+      still.classList.remove('active');
+      wDown.classList.add('active');
+      wUp.classList.remove('active');
    }
    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 }, false);
